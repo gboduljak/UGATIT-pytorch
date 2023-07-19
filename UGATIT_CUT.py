9bc287aab8905245c4557f48c6c0fc06a2d25ab4
Gabrijel Boduljak <gboduljak@outlook.com>
2023-07-19 05:37:25 +0200

    fix F_optim

--- UGATIT_CUT.py
+++ UGATIT_CUT.py
@@ -330,7 +330,7 @@ class UGATIT_CUT(object):
 
       # Update G
       self.G_optim.zero_grad()
-      self.optimizer_F.zero_grad()
+      self.F_optim.zero_grad()
 
       fake_A2B, fake_A2B_cam_logit, _ = self.genA2B(real_A)
       fake_B2B, fake_B2B_cam_logit, _ = self.genA2B(real_B)
